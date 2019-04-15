720dc04719e982f9ad498a8f698bbaaa77f140ff
ilte3 <ilte9605@gmail.com>
2019-04-15 22:55:11 +0300

    Update 11042019.R

--- Preparations/11042019.R
+++ Preparations/11042019.R
@@ -9,25 +9,23 @@ test_gen3 <- satf_gen(time = time, n = n, intercept = 0.5, rate = 1, asymptote =
 test_gen_bound <- rbind(test_gen, test_gen2, test_gen3)
 
 satf_calc <- function(data) {
-  names(data)[names(data) == "interval"] <- "bin"
+  data <- rename(data, bin = interval, lags = time)
   calc_hit <- data %>% filter(is_signal == 1) %>% group_by(condition, bin) %>% 
-              dplyr::summarize(hit = sum(response == TRUE)/sum(is_signal == 1))
+              dplyr::summarize(hit = sum(response == TRUE)/sum(is_signal == 1), lags = mean(lags))
   calc_miss <- data %>% filter(is_signal == 1) %>% group_by(condition, bin) %>% 
-               dplyr::summarize(miss = sum(response == FALSE)/sum(is_signal == 1))
+               dplyr::summarize(miss = sum(response == FALSE)/sum(is_signal == 1), lags = mean(lags))
   calc_fa <- data %>% filter(is_signal == 0) %>% group_by(condition, bin) %>% 
-             dplyr::summarize(fa = sum(response == TRUE)/sum(is_signal == 0))
+             dplyr::summarize(fa = sum(response == TRUE)/sum(is_signal == 0), lags = mean(lags))
   calc_creject <- data %>% filter(is_signal == 0) %>% group_by(condition, bin) %>% 
-                  dplyr::summarize(creject = sum(response == FALSE)/sum(is_signal == 0))
+                  dplyr::summarize(creject = sum(response == FALSE)/sum(is_signal == 0), lags = mean(lags))
   calc <- left_join(calc_hit, calc_miss) %>% left_join(calc_fa) %>% left_join(calc_creject)
   calc$hit.correction <- "none"
   calc$fa.correction <- "none"
-  colnames(calc) <- c("condition", "bin", "hit", "miss", "fa", "creject", "hit.correction", "fa.correction")
-  calc <- calc %>% select(bin, hit, hit.correction, fa, fa.correction, miss, creject, condition)
+  calc <- calc %>% select(bin, hit, hit.correction, fa, fa.correction, miss, creject, condition, lags )
   calc
 }
 
 test_calc <- satf_calc(data = test_gen_bound)
-View(test_calc)
 
 cmp_dprime <- function(data, hit, fa, miss, creject) {
   n_signal <- hit + miss
@@ -36,7 +34,6 @@ cmp_dprime <- function(data, hit, fa, miss, creject) {
   p_fa <- fa/n_noise
   data$dprimes <- qnorm(p_hit) - qnorm(p_fa)
   data$criterion <- -0.5*(qnorm(p_hit) + qnorm(p_fa))
-  data$lags <- runif(n = nrow(data), min = 0, max = 4.5) %>% sort
   drop <- c("miss", "creject", "criterion")
   data <- data[, !(names(data) %in% drop)] %>% 
           select(bin, hit, hit.correction, fa, fa.correction, lags, dprimes, condition) 
@@ -47,7 +44,33 @@ test_dprimes <- cmp_dprime(data = test_calc, hit = test_calc$hit, miss = test_ca
                            fa = test_calc$fa, creject = test_calc$creject)
 View(test_dprimes)
 
+### their data / start (for comparison)
 library(mrsat)
-pc311 <- list(asym=c(1, 2, 3), rate=c(1, 1, 1), incp=c(1, 1, 1))
-fit311 <- fit.SATcurve(test_dprimes, par.cond = pc311)
-plot(fit311, main = "311")
+
+data(Auditory_demo)
+
+my.signal <- list(noint = c(1,3), 
+                  obrel = c(5,8), 
+                  obrelsub = c(11, 14))
+my.noise <- list(noint = c(2,4), 
+                 obrel = c(6, 7, 9, 10), 
+                 obrelsub = c(12, 13, 15, 16))
+
+d.bins <- get.bins(Auditory_demo, auditory=TRUE)
+
+d.dprime <- get.dprime(d.bins$bins, signal.list=my.signal, noise.list=my.noise, 
+                       is.index=TRUE, binmax=14)
+
+View(d.dprime)
+
+### their data / end
+
+pc333 <- list(asym = c(1, 2, 3), rate = c(1, 2, 3), incp = c(1, 2, 3))
+fit333 <- fit.SATcurve(test_dprimes, par.cond = pc333)
+plot(fit333, main = "333")
+
+pc311 <- list(asym = c(1, 2, 3), rate = c(1, 1, 1), incp = c(1, 1, 1))
+fit.311 <- fit.SATcurve(test_dprimes, par.cond = pc311)
+plot(fit.311, main="311")
+
+SATsummary.list(list(fit.333, fit.311))
