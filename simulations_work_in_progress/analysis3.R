7ada78b27d469e0e27df7e4bb71c3fd805d12d72
Pavel Logacev <pavel.logacev@gmail.com>
2019-09-04 10:31:29 +0300

    Update.

--- simulations_work_in_progress/analysis3.R
+++ simulations_work_in_progress/analysis3.R
@@ -3,6 +3,7 @@ library(magrittr)
 library(ggpubr)
 
 df <- feather::read_feather(path = "all_simulations.feather")
+#df <- feather::read_feather(path = "../simulations/simulations_output_mr/simulation_50-20-0.700-0.800-3.000_0.000-0.000-0.000/all_simulations.feather")
 df222 <- df %>% filter(model == "2-2-2")
 
 n_participant_exp <- function(df) {
@@ -26,3 +27,16 @@ fun_t_test <- function(df) {
 
 df222 <- n_participant_exp(df222)
 t_tests_df <- as.data.frame(t(sapply(df222, FUN = fun_t_test)))
+
+
+################################################################################################################
+########### And this is what it should have been 
+################################################################################################################
+
+df222$group_id <- rep(1:(nrow(df222)/20), each = 20)
+
+t_tests_df <- 
+df222 %>% group_by(group_id) %>% 
+          dplyr::summarize( t_incp = t.test(intercept1, intercept2, paired = T, conf.level = 0.95)$statistic,
+                            t_rate = t.test(rate1, rate2, paired = T, conf.level = 0.95)$statistic,
+                            t_asymp = t.test(asymptote1, asymptote2, paired = T, conf.level = 0.95)$statistic )
